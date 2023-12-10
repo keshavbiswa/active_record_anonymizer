f6cea3a4eca13379f848ceb9731790f7eb61427c
Keshav Biswa <keshavbiswa21@gmail.com>
2023-12-10 11:05:40 +0530

    Initial config

--- test/active_record/test_anonymizer.rb
+++ test/active_record/test_anonymizer.rb
@@ -6,8 +6,4 @@ class ActiveRecord::TestAnonymizer < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ActiveRecord::Anonymizer::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
