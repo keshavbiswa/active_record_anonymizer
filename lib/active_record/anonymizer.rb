4c168bfd76ead022e1ac725cb0ac5279f6719d00
Keshav Biswa <keshavbiswa21@gmail.com>
2023-12-10 10:54:40 +0530

    initial commit

---
+++ lib/active_record/anonymizer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "anonymizer/version"
+
+module ActiveRecord
+  module Anonymizer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active_record/anonymizer/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ActiveRecord
+  module Anonymizer
+    VERSION = "0.1.0"
+  end
+end
