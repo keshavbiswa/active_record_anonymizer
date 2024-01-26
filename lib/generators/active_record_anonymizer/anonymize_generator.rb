7f6b3db9efef6ddb7972dc69cfc2d5a9036989cd
Keshav Biswa <keshavbiswa21@gmail.com>
2024-01-26 18:42:07 +0530

    removed validation for anonymize generator

--- lib/generators/active_record_anonymizer/anonymize_generator.rb
+++ lib/generators/active_record_anonymizer/anonymize_generator.rb
@@ -7,16 +7,13 @@ module ActiveRecordAnonymizer
   class AnonymizeGenerator < ActiveRecord::Generators::Base
     include Rails::Generators::Migration
 
-    class InvalidModelName < Thor::Error; end
     class InvalidArguments < Thor::Error; end
-    class InvalidModel < Thor::Error; end
 
     source_root File.expand_path("templates", __dir__)
 
     argument :attributes, type: :array, default: [], banner: "attribute attribute"
 
     def generate_migration
-      validate_model_name
       validate_arguments
       migration_template "migration.rb.erb", "db/migrate/anonymize_#{table_name}.rb"
       output_instructions
@@ -36,25 +33,8 @@ module ActiveRecordAnonymizer
       "  anonymize :#{attributes.map(&:name).join(', :')}\n"
     end
 
-    def validate_model_name
-      raise InvalidModelName, "Invalid model name" unless model
-    end
-
     def validate_arguments
       raise InvalidArguments, "Attributes are needed for the migration" if attributes.empty?
-
-      attributes.each do |attribute|
-        raise InvalidArguments, "#{attribute.name} is not a valid attribute for #{model}" unless model.columns_hash[attribute.name]
-      end
-    end
-
-    def model
-      klass = class_name.safe_constantize
-      if klass.respond_to?(:connection) && klass.connection.table_exists?(klass.table_name)
-        klass
-      else
-        raise InvalidModel, "#{class_name} is not a valid ActiveRecord model with a corresponding table"
-      end
     end
 
     def migration_version
